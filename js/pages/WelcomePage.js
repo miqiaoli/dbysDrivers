1cd941c6fe357d86de21318792a70df0082008d2
ivey <756994691@qq.com>
2019-05-29 14:29:08 +0800

    版本提升

--- js/pages/WelcomePage.js
+++ js/pages/WelcomePage.js
@@ -33,7 +33,7 @@ export default class HomePage extends Component<Props> {
         constructor(props) {
             super(props)
             this.state = {
-                appVersion:'1.1.4'
+                appVersion:'1.1.5'
             }
         }
         async componentDidMount() {
